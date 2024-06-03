1ad28308e3d2114bab33e821743ded26289ded09
D-a-x-M <medaniel27@gmail.com>
2024-06-03 14:04:39 -0400

    Entorno virtual

--- Redactor Mensajes.bat
+++ Redactor Mensajes.bat
@@ -4,6 +4,7 @@ rem Cambiar directorio a la ubicación del archivo Python
 cd "./utils"
 
 rem Ejecutar el archivo Python
+call venv/Scripts/activate
 python redactorMensajes.py
 
 rem (Opcional) Pausar la ejecución del bat después de ejecutar el Python
