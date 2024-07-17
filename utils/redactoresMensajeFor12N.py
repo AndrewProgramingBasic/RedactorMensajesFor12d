c38188a3b788ef8d9377186a9663858c3e0cd44f
AndrewProgramingBasic <andradeandradeje@gmail.com>
2024-07-17 18:31:48 -0400

    Cambios en modo de guardado

--- utils/redactoresMensajeFor12N.py
+++ utils/redactoresMensajeFor12N.py
@@ -66,6 +66,7 @@ def redactorFor12dNormal(archivo,cdc):
     todosLosMensajes += messageFinal
 
     # Guardar los mensajes en un archivo de texto
-    guardar_en_txt(todosLosMensajes, "../"+cdc+".txt")
+    guardar_en_txt(todosLosMensajes, "../CDC/"+cdc+".txt")
+    guardar_en_txt(todosLosMensajes, "../salida.txt")
 
-    print("¡Texto guardado en el archivo "+cdc+".txt!")
+    print("¡Texto guardado en el archivo "+cdc+".txt en la carpeta de CDC y en el archivo salida!")
